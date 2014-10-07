b3106d8934ce8e3f3c77c944c9c96355f03b0def
Robert St. John <stjohnr@bit-sys.com>
2014-10-07 15:22:36 -0600

    pushing to github

---
+++ Storyboard/GreenViewController.h
@@ -0,0 +1,15 @@
+//
+//  GreenViewController.h
+//  Storyboard
+//
+//  Created by Robert St. John on 8/24/14.
+//  Copyright (c) 2014 BIT Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GreenViewController : UIViewController
+
+- (IBAction)btnClose:(id)sender;
+
+@end
